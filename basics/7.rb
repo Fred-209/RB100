1df1f0073f568b3fa4bde43bed75d477d3d20d71
Fred Durham <f.durham@protonmail.com>
2021-04-29 06:29:53 -0500

    Added Exercise 7.rb

---
+++ basics/7.rb
@@ -0,0 +1,11 @@
+=begin
+The error
+
+"SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'"  
+
+tells me that a ')' was used where Ruby was expecting a '}' to be, or the '}' was 
+forgotten at the end of a block of code.  
+
+  
+=end
