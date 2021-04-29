bd2b8e74b193072d6f8feb28e74fe1d3508d41a1
Fred Durham <f.durham@protonmail.com>
2021-04-29 06:14:17 -0500

    Added Exercise 3

---
+++ basics/3.rb
@@ -0,0 +1,15 @@
+# Hash of movie names with year they were released
+movie_list = {
+  The_Big_Lebowski: 1998,
+  Batman: 1989,
+  Kids: 1995,
+  Mortal_Kombat: 2021,
+  Get_Out: 2017
+}
+
+# Puts the year each movie was released 
+puts movie_list[:The_Big_Lebowski]
+puts movie_list[:Batman]
+puts movie_list[:Kids]
+puts movie_list[:Mortal_Kombat]
+puts movie_list[:Get_Out]
