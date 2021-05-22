2afa5a39e4d988dccf99d09f3f75d570a84de61a
Fred Durham <f.durham@protonmail.com>
2021-05-22 11:28:17 -0500

    Added some exercises from the conditionals section

---
+++ conditionals/truthy_number.rb
@@ -0,0 +1,11 @@
+# What will the following code print? Don't run it until you've attempted to answer.
+
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+# I think it will print 'My favorite number is 7." because 'if number' evaluates to true
